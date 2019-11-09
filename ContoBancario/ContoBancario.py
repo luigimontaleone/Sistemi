62bdd3987650a0c43de9c8b43fcea55507c88b8b
monta <monta@DESKTOP-GU0NTK5>
2019-11-09 23:35:52 +0100

    risolto 0.25

--- ContoBancario/ContoBancario.py
+++ ContoBancario/ContoBancario.py
@@ -1,7 +1,14 @@
-from queue import Queue
 from Transazione import Transazione
+from threading import RLock
 class ContoBancario:
-    def __init__(self, saldo : int):
-        self.saldo = saldo
-        self.transazioni = Queue(50)
-    
+    ID = 1
+    def __init__(self, s : int):
+        self.saldo = s
+        self.transazioni = list()
+        self.id = ContoBancario.ID
+        ContoBancario.ID += 1
+        self.lock = RLock()
+    def addTransazione(self, t : Transazione):
+        if(len(self.transazioni) == 50):
+            self.transazioni.pop(0)
+        self.transazioni.append(t)
--- ContoBancario/Transazione.py
+++ ContoBancario/Transazione.py
@@ -1,7 +1,6 @@
-from ContoBancario import ContoBancario
 class Transazione:
-    def __init__(self, s : ContoBancario, d : ContoBancario, importo : int):
-        self.sorgente = d
+    def __init__(self, s, d, importo : int):
+        self.sorgente = s
         self.destinazione = d
         self.importo = importo
         
