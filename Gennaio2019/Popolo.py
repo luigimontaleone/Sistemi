e3bf1eeb846d5cc34868feb91637d120892a8b7f
monta <monta@DESKTOP-GU0NTK5>
2019-12-29 14:35:13 +0100

    comment left

--- Gennaio2019/Popolo.py
+++ Gennaio2019/Popolo.py
@@ -8,13 +8,6 @@ class Popolo:
     def assegnaCittadini(self, cittadiniList):
         self.cittadini = cittadiniList
     def distribuisciReddito(self):
-        #
-        # Attribuisce a tutti i componenti di self.cittadini il reddito del mese corrente (780 EUR a testa), 
-        # decrementando
-        # soldiDisponibili e incrementando soldiErogati. Genera una eccezione e interrompe l'operazione 
-        # se 
-        # durante l'operazione i soldiDisponibili dovessero finire
-        #
         with self.lock:
             for c in self.cittadini:
                 if self.soldiDisponibili - 780 >= 0:
@@ -24,16 +17,9 @@ class Popolo:
                 else:
                     print(f"Errore: soldi disponibili = {self.soldiDisponibili}")      
     def aggiungiSoldi(self, valore : int):
-        #
-        # incrementa self.soldiDisponibili dell'ammontare di 'valore'
-        #
         with self.lock:
             self.soldiDisponibili += valore
     def iContiTornano(self):
-        #
-        # Verifica che la somma di quanto percepito dai singoli elementi di self.cittadini corrisponda a self.soldiErogati
-        # restituisce un valore booleano in accordo
-        #
         with self.lock:
             somma = 0
             for c in self.cittadini:
