cd48cd2a7882b62147875d360f2bbfcdc6070174
luigi <luigi@luigi-pc>
2019-12-29 13:41:24 +0100

    ended

---
+++ Settembre2018/BlockingStack.py
@@ -0,0 +1,24 @@
+from threading import RLock, Condition
+class BlockingStack:
+    def __init__(self, dim):
+        self.dim = dim
+        self.blockingStack = []
+        self.lock = RLock()
+        self.condition = Condition(self.lock)
+    def put(self, t):
+        with self.lock:
+            while len(self.blockingStack >= self.dim):
+                self.condition.wait()
+            self.blockingStack.append(t)
+            self.condition.notifyAll()
+    def take(self, t = 0):
+        self.lock.acquire()
+        while t not in self.blockingStack:
+            self.condition.wait()
+        try:
+            return self.blockingStack.pop(self.blockingStack.index(t))
+        finally:
+            self.condition.notifyAll()
+            self.lock.release()
+            
+    
